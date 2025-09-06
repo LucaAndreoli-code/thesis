14cb9d1d0d0596b3b99d1f9b81752bb7941fd3c9
LucaAndreoli-code <lucandreoli3@gmail.com>
2025-09-06 13:42:15 +0200

    resetting database when launching tests

---
+++ simplePay-be/src/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import sys
+from sqlalchemy import text
+
+from migration_manager import setup_database
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.database.database import engine
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_database():
+    print("Reset database for tests...")
+
+    with engine.connect() as conn:
+        conn.execute(text("DROP SCHEMA IF EXISTS public CASCADE"))
+        conn.execute(text("CREATE SCHEMA public"))
+        conn.commit()
+        setup_database()
+
+    yield
