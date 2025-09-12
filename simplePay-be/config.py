c4b85880805a7d26adce9a9b251a0de1079dba11
LucaAndreoli-code <lucandreoli3@gmail.com>
2025-09-12 11:40:16 +0200

    test deploy

--- simplePay-be/config.py
+++ simplePay-be/config.py
@@ -9,12 +9,5 @@ else:
     print("Loading DEFAULT environment variables...")
     load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5433")
-DB_NAME = os.getenv("DB_NAME", "postgres")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres")
 SECRET_KEY = os.getenv("SECRET_KEY")
